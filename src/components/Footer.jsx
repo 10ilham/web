4a6201a4a7161adef7e5ed02db2117323a18caf8
10ilham <ilhammunawar55@gmail.com>
2023-12-08 18:49:32 +0700

    y

--- src/components/Footer.jsx
+++ src/components/Footer.jsx
@@ -1,25 +1,29 @@
-import Container from 'react-bootstrap/Container';
-import  Col from 'react-bootstrap/Col';
-import Row from 'react-bootstrap/Row';
-import logo from '../assets/img/astro-indo.svg';
-
+import Container from "react-bootstrap/Container";
+import Col from "react-bootstrap/Col";
+import Row from "react-bootstrap/Row";
+// import logo from '../assets/img/ilhamm.jpg';
+import logo from "../assets/logo/ilhamm.jpg";
 
 export const Footer = () => {
-    return (
-      <footer className="footer">
-        <Container>
-          <Row className="align-items-center">
-            <Col size={12} sm={1} className='flex justify-center mt-3'>
-              <img src={logo} className='rounded-full border'/>
-            </Col>
-            <Col size={12} sm={7} className='text-center text-sm-start mt-3 font-semibold '>
-                <p>Ilham Munawar</p>
-            </Col>
-            <Col size={12} sm={4} className="text-center text-sm-end mt-3 ">
-              <p>Copyright 2023</p>
-            </Col>
-          </Row>
-        </Container>
-      </footer>
-    )
-  }
+  return (
+    <footer className="footer">
+      <Container>
+        <Row className="align-items-center">
+          <Col size={12} sm={1} className="flex justify-center mt-3">
+            <img src={logo} className="rounded-full border" />
+          </Col>
+          <Col
+            size={12}
+            sm={7}
+            className="text-center text-sm-start mt-3 font-semibold "
+          >
+            <p>Ilham Munawar</p>
+          </Col>
+          <Col size={12} sm={4} className="text-center text-sm-end mt-3 ">
+            <p>Copyright 2023</p>
+          </Col>
+        </Row>
+      </Container>
+    </footer>
+  );
+};
